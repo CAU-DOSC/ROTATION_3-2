fbd715d2af5b4c251e290ba02a9f9e533e319d14
SeongYunKim <cauboy17@naver.com>
2017-12-05 16:02:23 +0900

    Add Header

---
+++ Stringsort_Head.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#pragma warning(disable: 4996)
+
+char *trivial(char str[], int n, int d);
+char *juggling(char str[], int n, int d);
+char *blockswap(char str[], int n, int d);
+char *reverse(char str[], int n, int d);
