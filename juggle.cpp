6dc2a52ef1bc71e913bcbc9a9035659990dd7f54
celi1004 <33647623+celi1004@users.noreply.github.com>
2017-12-07 13:12:33 +0900

    upload juggle.cpp

---
+++ juggle.cpp
@@ -0,0 +1,62 @@
+//#include "Stringsort_Head.h"
+//#include <math.h>
+
+//최대공약수 찾는 함수
+/*int getGCD(int a, int b) {
+	while (0 != b) {
+		int temp = a % b;
+		a = b;
+		b = temp;
+	}
+	return abs(a);
+}
+
+char *juggling(char str[], int n, int d)
+{
+	int i, j, k, temp;
+
+	// 시계방향으로 rotate
+	if (d >= 0) {
+		for (i = 0; i < getGCD(d, n); i++)
+		{
+			
+			temp = str[i];
+			j = i;
+			while (1)
+			{
+				k = j - d;
+				if (k < 0)
+					k = k + n;
+				if (k == i)
+					break;
+				str[j] = str[k];
+				j = k;
+			}
+			str[j] = temp;
+		}
+	}
+
+	//반시계방향으로 rotate
+	if (d < 0) {
+		for (i = 0; i < getGCD(d, n); i++)
+		{
+			
+			temp = str[i];
+			j = i;
+			while (1)
+			{
+				k = j - d;
+				if (k >= n )
+					k = k - n;
+				if (k == i)
+					break;
+				str[j] = str[k];
+				j = k;
+			}
+			str[j] = temp;
+		}
+	}
+
+	return str;
+}
+*/
