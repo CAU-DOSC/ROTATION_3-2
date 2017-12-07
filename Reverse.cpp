f210a995dd872e8062e42a3005c6224755675aff
SeongYunKim <cauboy17@naver.com>
2017-12-07 17:01:45 +0900

    Remove duplication

--- Reverse.cpp
+++ Reverse.cpp
@@ -49,20 +49,6 @@ char *reverse(char str[], int n, int d)
 		for (int b = 0; b < d; b++)
 			str[b + n - d] = str2[b];
 		strrev(str, n);
-
-		str1 = (char*)malloc(sizeof(char) * ((n - d) + 1));
-		str2 = (char*)malloc(sizeof(char) * (n + 1));
-		for (int a = 0; a < n - d; a++)
-			str1[a] = str[a];
-		for (int b = 0; b < d; b++)
-			str2[b] = str[b + n - d];
-		strrev(str1, n - d);
-		for (int a = 0; a < n - d; a++)
-			str[a] = str1[a];
-		strrev(str2, d);
-		for (int b = 0; b < d; b++)
-			str[b + n - d] = str2[b];
-		strrev(str, n);
 	}
 	return str;
 }
