8ef2ffe8956bf188d5f15e4eab9b886f7d6335f9
SeongYunKim <cauboy17@naver.com>
2017-12-07 18:19:27 +0900

    Restruct reverse function

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,55 +1,33 @@
 #include "Stringsort_Head.h"
 
-
-void strrev(char str[], int length)
+void strrev(char str[], int start, int end)
 {
 	char temp = 0;
-	for (int start = 0, end = length - 1; start < end; start++, end--)
+	for (int i = 0; i < ((end - start + 1) / 2); i++)
 	{
-		temp = str[start];
-		str[start] = str[end];
-		str[end] = temp;
+		temp = str[start + i];
+		str[start + i] = str[end - i];
+		str[end - i] = temp;
 	}
 }
 
 //Reverse 
 char *reverse(char str[], int n, int d)
 {
-	char *str1, *str2;
-	//CounterClockWise Rotation(¹Ý½Ã°è¹æÇâ)
+	//CounterClockWise Rotation(반시계방향)
 	if (d < 0)
 	{
 		d = -d;
-		str1 = (char*)malloc(sizeof(char) * (d + 1));
-		str2 = (char*)malloc(sizeof(char) * (n - d + 1));
-		for (int a = 0; a < d; a++)
-			str1[a] = str[a];
-		for (int b = 0; b< n - d; b++)
-			str2[b] = str[b + d];
-		strrev(str1, d);
-		for (int a = 0; a < d; a++)
-			str[a] = str1[a];
-		strrev(str2, n - d);
-		for (int b = 0; b< n - d; b++)
-			str[b + d] = str2[b];
-		strrev(str, n);
+		strrev(str, 0, d - 1);
+		strrev(str, d, n - 1);
+		strrev(str, 0, n - 1);
 	}
-	//ClockWise Rotation(½Ã°è¹æÇâ)
+	//ClockWise Rotation(시계방향)
 	else
 	{
-		str1 = (char*)malloc(sizeof(char) * ((n - d) + 1));
-		str2 = (char*)malloc(sizeof(char) * (n + 1));
-		for (int a = 0; a < n - d; a++)
-			str1[a] = str[a];
-		for (int b = 0; b < d; b++)
-			str2[b] = str[b + n - d];
-		strrev(str1, n - d);
-		for (int a = 0; a < n - d; a++)
-			str[a] = str1[a];
-		strrev(str2, d);
-		for (int b = 0; b < d; b++)
-			str[b + n - d] = str2[b];
-		strrev(str, n);
+		strrev(str, n - d, n - 1);
+		strrev(str, 0, n - d - 1);
+		strrev(str, 0, n - 1);
 	}
 	return str;
 }
