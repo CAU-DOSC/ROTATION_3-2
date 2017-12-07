439c95b73f9a06aab403c8e87ef93554e4a7d8fe
ClearT <clear1014@gmail.com>
2017-12-07 19:19:39 +0900

    change clock/counterclockwise

--- blockswap.cpp
+++ blockswap.cpp
@@ -14,7 +14,7 @@ void strswap(char *str1, char *str2, int d)
 	return;
 }
 
-//str은 앞 문자열 맨 앞의 주소값, len은 문자열의 길이, d는 앞부분 문자열의 길이
+//str은 앞 문자열 맨 앞의 주소값, len은 문자열의 길이, d는 뒷부분 문자열의 길이
 char* blockswap(char str[], int len, int d)
 {
 	if (d < 0) d = len + d;	//d가 0보다 작으면counterclockwise
@@ -30,18 +30,18 @@ char* blockswap(char str[], int len, int d)
 		strswap(str, str+d, d);
 		return str;
 	}
-	//앞 문자열이 더 짧을 때
+	//앞 문자열이 더 길 때
 	if (2 * d < len)
 	{
 		strswap(str, str + len - d, d);
-		blockswap(str, len - d, d);
+		blockswap(str + d, len - d, d);
 		return str; 
 	}
-	//앞 문자열이 더 길 때
+	//앞 문자열이 더 짧을 때
 	if (2 * d > len)	
 	{
-		strswap(str, str + d, len - d);
-		blockswap(str + len - d, d, 2 * d - len);
+		strswap(str, str +len - d, len - d);
+		blockswap(str, d, 2 * d - len);
 		return str;
 	}
 }
